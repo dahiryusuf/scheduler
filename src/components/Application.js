d80db5b077230b297ecfe79be3a16634a5260173
dahiryusuf <dahir.yusuf@hotmail.com>
2021-12-15 01:38:31 -0500

    Add funtions in selector

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import DayList from "components/DayList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
 import axios from 'axios'
-import { getAppointmentsForDay,getInterview } from "helpers/selectors";
+import { getAppointmentsForDay,getInterview,getInterviewersForDay } from "helpers/selectors";
 
 
 // const appointments = [
@@ -62,11 +62,13 @@ export default function Application(props) {
       axios.get('http://localhost:8001/api/appointments'),
       axios.get('http://localhost:8001/api/interviewers')
     ]).then((all) => {
+     
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
       
     });
   }, []);
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const dailyInterviewers = getInterviewersForDay(state, state.day)
 
   const schedule = dailyAppointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
@@ -77,6 +79,7 @@ export default function Application(props) {
         id={appointment.id}
         time={appointment.time}
         interview={interview}
+        interviewers={dailyInterviewers}
       />
     );
   });
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -11,6 +11,18 @@ export function getAppointmentsForDay(state, day) {
  return appointments
 }
 
+export function getInterviewersForDay(state, day) {
+  const filteredDays = state.days.filter(days => days.name === day);
+  if(filteredDays.length === 0){
+    return []
+  }
+  const interviewers = filteredDays[0].interviewers.map(int => {  
+    return state.interviewers[int]
+  });
+
+ return interviewers
+}
+
 export function getInterview(state, interview) {
   if(!interview){
     return null
